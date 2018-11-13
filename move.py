5bceacf6ddf7d354ff93d8045715f30ec2f97c82
ExcelCyphernia <kai@enoshlo.com>
2018-11-13 14:35:45 +0200

    added examples folder

--- move.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from ev3dev2.sound import Sound
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
-sound = Sound()
-sound.speak('starting')
-tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(50))
